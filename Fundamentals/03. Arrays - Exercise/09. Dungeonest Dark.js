fffdf264733433ae59f7a6dac191ff5cea459b6a
Tihomir13 <tihomir13n2002@abv.bg>
2023-10-02 09:03:04 +0300

    updating 9

--- Fundamentals/03. Arrays - Exercise/09. Dungeonest Dark.js
+++ Fundamentals/03. Arrays - Exercise/09. Dungeonest Dark.js
@@ -1,1 +1,18 @@
-ds;
+function DungeonestDark(string) {
+  let health = 100;
+  let coins = 0;
+  let arr = [];
+  let currentRoom = ``;
+
+  for (let i = 0; i < string.length; i++) {
+    currentRoom += string;
+    if (string[i].includes(`|`)) {
+      i++;
+      arr.push(currentRoom);
+      currentRoom = ` `;
+    }
+  }
+  console.log(arr);
+  console.log(string.split(` `));
+}
+DungeonestDark(["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"]);
