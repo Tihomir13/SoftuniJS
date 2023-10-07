087fbfb1284038c9d7ff49a6c425e6282000a426
Tihomir13 <tihomir13n2002@abv.bg>
2023-10-08 00:16:54 +0300

    update

--- Fundamentals/04.Functions - Exercise/01. Smallest of Three Numbers.js
+++ Fundamentals/04.Functions - Exercise/01. Smallest of Three Numbers.js
@@ -1,5 +1,5 @@
 function SmallestofThreeNumbers(a, b, c) {
     if (a < b && a < c) console.log(a);
-    else if (c < b && c < a) console.log(a);
-    else if (b < c && b < a) console.log(a);
+    else if (c < b && c < a) console.log(c);
+    else if (b < c && b < a) console.log(b);
 }
