fdae9eb512ecd522e7667c81bb27813db9c5ed94
Tihomir13 <tihomir13n2002@abv.bg>
2023-10-28 17:21:31 +0300

    updating 6th exercise

---
+++ Fundamentals/06. Objects and Classes/01. Employees.js
@@ -0,0 +1,16 @@
+function Employees(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let employee = {
+            print(name) {
+                console.log(`Name: ${name} -- Personal Number: ${name.length}`);
+            }
+        }
+        employee.print(arr[i]);
+    }
+}
+
+Employees(['Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+]);
