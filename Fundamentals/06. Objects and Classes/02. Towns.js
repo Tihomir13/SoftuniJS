7c6242483375b99a8f3d0e1e4fbf1c566b927c14
Tihomir13 <tihomir13n2002@abv.bg>
2023-10-28 18:18:30 +0300

    exercise 2

--- Fundamentals/06. Objects and Classes/02. Towns.js
+++ Fundamentals/06. Objects and Classes/02. Towns.js
@@ -1,3 +1,14 @@
-function Towns() {
-
+function Towns(arr) {
+    for (let info of arr) {
+        let token = info.split(` |`);
+        town = {
+            name: token[0],
+            latitude: Number(token[1]),
+            longitude: Number(token[2]),
+        }
+        console.log(`{ town: '${town.name}', latitude: '${town.latitude.toFixed(2)}', longitude: '${town.longitude.toFixed(2)}' }`);
+    }
 }
+
+Towns(['Sofia | 42.696552 | 23.32601',
+    'Beijing | 39.913818 | 116.363625']);
