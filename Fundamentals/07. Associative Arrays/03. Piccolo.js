7172f8955667c16e688b37266b93efe8e439f207
Tihomir13 <tihomir13n2002@abv.bg>
2023-11-11 00:08:46 +0200

    update

--- Fundamentals/07. Associative Arrays/03. Piccolo.js
+++ Fundamentals/07. Associative Arrays/03. Piccolo.js
@@ -17,13 +17,3 @@ function Piccolo(arr) {
         console.log(`${Number}`);
     }
 }
-Piccolo(['IN, CA2844AA',
-    'IN, CA1234TA',
-    'OUT, CA2844AA',
-    'IN, CA9999TT',
-    'IN, CA2866HI',
-    'OUT, CA1234TA',
-    'IN, CA2844AA',
-    'OUT, CA2866HI',
-    'IN, CA9876HH',
-    'IN, CA2822UU']);
