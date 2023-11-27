d13be0783b5bbf07fa9b2a597b1a0834b4491801
Tihomir13 <tihomir13n2002@abv.bg>
2023-11-27 12:30:02 +0200

    update

--- Fundamentals/Подготовка за Изпит/02. AdAstra.js
+++ Fundamentals/Подготовка за Изпит/02. AdAstra.js
@@ -3,7 +3,6 @@ function AdAstra(arr) {
     let items = [];
     let totalCalories = 0;
     let pattern = /([|#])(?<foodName>[A-z\s]+)\1(?<experationDate>\d{2}\/\d{2}\/\d{2})\1(?<calories>\d+)\1/g;
-
     let matches = text.matchAll(pattern);
 
     for (let match of matches) {
@@ -17,5 +16,3 @@ function AdAstra(arr) {
 
     items.forEach(item => console.log(`Item: ${item.foodName}, Best before: ${item.experationDate}, Nutrition: ${item.calories}`));
 }
-
-AdAstra(['#Bread#19/03/21#4000#|Invalid|03/03.20||Apples|08/10/20|200||Carrots|06/08/20|500||Not right|6.8.20|5|']);
