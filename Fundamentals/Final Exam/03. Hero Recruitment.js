6c357c283686dc0824a6bf42fc61e0bfcf99a740
Tihomir13 <tihomir13n2002@abv.bg>
2023-12-03 12:13:48 +0200

    update

--- Fundamentals/Final Exam/03. Hero Recruitment.js
+++ Fundamentals/Final Exam/03. Hero Recruitment.js
@@ -11,17 +11,16 @@ function HeroRecruitment(arr) {
         }
         else if (command == `Learn`) {
             if (name in heroes)
-                if (heroes[name].spells.includes(spell))//
+                if (heroes[name].spells.includes(spell))
                     console.log(`${name} has already learnt ${spell}.`);
                 else
-                    heroes[name].spells.push(spell);//
+                    heroes[name].spells.push(spell);
             else
                 console.log(`${name} doesn't exist.`);
         }
         else if (command == `Unlearn`) {
             if (name in heroes) {
-                if (heroes[name].spells.includes(spell))//
-                {
+                if (heroes[name].spells.includes(spell)) {
                     let index = heroes[name].spells.indexOf(spell);
                     heroes[name].spells.splice(index, 1);
                 }
@@ -33,34 +32,8 @@ function HeroRecruitment(arr) {
         }
         [command, name, spell] = arr.shift().split(` `);
     }
-    let entries = Object.entries(heroes);
-    if (heroes) {
-        console.log(`Heroes:`);
-        for (let [name, property] of entries) {
-            console.log(`==${name}: property`);
-        }
-
-    }
+    console.log('Heroes:');
+    Object.keys(heroes).forEach(name => {
+        console.log(`== ${name}: ${heroes[name].spells.join(', ')}`);
+    });
 }
-HeroRecruitment([
-    "Enroll Stefan",
-    "Enroll Peter",
-    "Enroll Stefan",
-    "Learn Stefan ItShouldWork",
-    "Learn John ItShouldNotWork",
-    "Unlearn George Dispel",
-    "Unlearn Stefan ItShouldWork",
-    "End"]);
-
-// HeroRecruitment(["Enroll Stefan",
-//     "Learn Stefan ItShouldWork",
-//     "Learn Stefan ItShouldWork",
-//     "Unlearn Stefan NotFound",
-//     "End"]);
-
-// HeroRecruitment(["Enroll Stefan",
-//     "Enroll Peter",
-//     "Enroll John",
-//     "Learn Stefan Spell",
-//     "Learn Peter Dispel",
-//     "End"])
