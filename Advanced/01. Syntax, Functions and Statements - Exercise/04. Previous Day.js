200c571b853c3f37a7e177dd795f342a78cd6e21
Tihomir13 <64681422+Tihomir13@users.noreply.github.com>
2024-01-13 12:36:37 +0200

    Delete Advanced/01. Syntax, Functions and Statements - Exercise directory

--- Advanced/01. Syntax, Functions and Statements - Exercise/04. Previous Day.js
+++
@@ -1,5 +0,0 @@
-function PreviousDay(year, month, day) {
-    let date = new Date(year, month - 1, day);
-    date.setDate(day - 1);
-    console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
-}
